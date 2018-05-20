a47d3f62d0e008a60acde7c6cd626aaf9e115782
Sergey Denisov <xsergeantx@mail.ru>
2018-05-20 19:30:28 +0300

    chore: Use babel

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,16 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      },
+      {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
           use: ["css-loader", "postcss-loader"]
